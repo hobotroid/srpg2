addddad5c41e2ef2b94f6cfc8697c9993275d530
hobotroid <hobotroid@gmail.com>
2013-07-09 10:21:36 -0400

    more

--- Main/src/com/lasko/srpg/map/Map.java
+++ Main/src/com/lasko/srpg/map/Map.java
@@ -1,7 +1,11 @@
 package com.lasko.srpg.map;
 
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Map
 {
@@ -14,10 +18,15 @@ public class Map
     private float tileWidth;
     private float tileHeight;
 
+    public int[] layersPlayer = {};
+    public int[] layersAbove = {};
+    public int[] layersBelow = {};
+
     public Map(TiledMap map)
     {
         this.map = map;
 
+        //store some crucial info
         TiledMapTileLayer firstLayer = (TiledMapTileLayer)map.getLayers().get(0);
         this.widthInTiles = firstLayer.getWidth();
         this.heightInTiles = firstLayer.getHeight();
@@ -25,6 +34,24 @@ public class Map
         this.heightInPixels = firstLayer.getHeight() * firstLayer.getTileHeight();
         this.tileWidth = firstLayer.getTileWidth();
         this.tileHeight = firstLayer.getTileHeight();
+
+        //figure out which layers are which
+        Iterator<MapLayer> layerIterator = map.getLayers().iterator();
+        int index = 0;
+        while(layerIterator.hasNext()) {
+            MapLayer layer = layerIterator.next();
+            String layerName = layer.getName().toLowerCase();
+            System.out.println(layerName);
+            if(layerName.equals("player")) {
+                layersPlayer = addElement(layersPlayer, index);
+            } else if(layerName.equals("above")) {
+                layersAbove = addElement(layersAbove, index);
+            } else {
+                layersBelow = addElement(layersBelow, index);
+            }
+
+            index++;
+        }
     }
 
     public TiledMap getMap()
@@ -60,5 +87,16 @@ public class Map
     public float getTileHeight()
     {
         return tileHeight;
+    }
+
+    public float getTileWidthLarge()
+    {
+        return 48f;
+    }
+
+    int[] addElement(int[] org, int added) {
+        int[] result = Arrays.copyOf(org, org.length +1);
+        result[org.length] = added;
+        return result;
     }
 }
--- Main/src/com/lasko/srpg/map/MapRenderer.java
+++ Main/src/com/lasko/srpg/map/MapRenderer.java
@@ -4,29 +4,26 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-/**
- * Created with IntelliJ IDEA.
- * User: John Windows
- * Date: 7/8/13
- * Time: 1:04 PM
- * To change this template use File | Settings | File Templates.
- */
 public class MapRenderer
 {
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
+    private Map map;
 
     public MapRenderer(Map map, OrthographicCamera camera)
     {
         float unitScale = 1 / map.getTileWidth();   //we want 1 unit to equal 1 tile
         renderer = new OrthogonalTiledMapRenderer(map.getMap(), unitScale);
         this.camera = camera;
+        this.map = map;
     }
 
     public void render()
     {
         camera.update();
         renderer.setView(camera);
-        renderer.render();
+        renderer.render(map.layersBelow);
+        renderer.render(map.layersPlayer);
+        renderer.render(map.layersAbove);
     }
 }
